977732043f93ae6c29fb57ff6d59d5ac821264fc
salahqasm <salahqasm@outlook.com>
2023-06-01 23:11:01 +0300

    done

--- script.js
+++ script.js
@@ -1,11 +1,20 @@
 let form = document.getElementById("eval");
-form.addEventListener('submit', getData);
+form.addEventListener('submit',getData);
 
 function getData(e) {
-    e.preventDefault();
+
+    e.preventDefault(); //preventing refresh after submission
+    
     let answers = {};
+
     for (let i = 1; i <= 11; i++) {
         answers[`q${i}`]=e.target[`q${i}`].value;
     }
-    console.log(answers);
+    
+    console.log(answers); //logging answers 
+
+    window.alert("Evaluation Submitted, Thank you") //alert
+
+    form.reset(); //clearing the form after submission
+
 }
